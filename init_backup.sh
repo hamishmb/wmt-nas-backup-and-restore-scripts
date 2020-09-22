0e18938228884cd3dceaed8e487e4da5c13543d7
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-09-22 11:55:13 +0100

    Set PATH and attempt to start a backup in the backup init script

--- init_backup.sh
+++ init_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
 PATH=/mnt/HD/HD_a2/nas-sysroot/usr/local/bin:/mnt/HD/HD_a2/nas-sysroot/usr/local/sbin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 
-python3 /mnt/HD/HD_a2/backupandrestore/backup.py > /mnt/HD/HD_a2/backupandrestore/log.log
+/mnt/HD/HD_a2/nas-sysroot/usr/local/bin/python3 /mnt/HD/HD_a2/backupandrestore/backup.py > /mnt/HD/HD_a2/backupandrestore/log.log 2> /mnt/HD/HD_a2/backupandrestore/log.log
